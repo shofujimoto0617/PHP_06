6d8a74c45609901d8668029e2da9d6f5de06e5df
shofujimoto0617 <shofujimoto017@gmail.com>
2021-01-06 12:31:20 +0900

    Update to check.php

--- join/check.php
+++ join/check.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['join'])) {
+	header('Location: index.php');
+	exit();
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -21,9 +31,11 @@
 	<dl>
 		<dt>ニックネーム</dt>
 		<dd>
+		<?php print(htmlspecialchars($_SESSION['join'] ['name'], ENT_QUOTES)); ?>
         </dd>
 		<dt>メールアドレス</dt>
 		<dd>
+		<?php print(htmlspecialchars($_SESSION['join'] ['email'], ENT_QUOTES)); ?>
         </dd>
 		<dt>パスワード</dt>
 		<dd>
